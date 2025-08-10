afb6f99314f3d71c42623d2b76b40cd040e7ae8e
mncc8337 <mncc8327@gmail.com>
2025-08-10 22:29:08 +0700

    temp controll box

--- esp32/temperature_control/src/main.cpp
+++ esp32/temperature_control/src/main.cpp
@@ -1,84 +1,100 @@
 #include <Arduino.h>
-#include <Adafruit_BMP280.h>
-#include <SPI.h>
+#include <Wire.h>
+#include <LCDI2C_Multilingual_MCD.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-#include <math.h>
+#define IIC_SCL 22
+#define IIC_SDA 21
 
-#define LAMP 17
+#define LAMP 18
+#define ONE_WIRE 19
 
-#define HSPI_SCK 14
-#define HSPI_MOSI 13
-#define HSPI_MISO 12
+#define INTERVAL 800
+#define MIN_TEMP 30.5
 
-#define BMP_CS 15
+LCDI2C_Generic lcd(0x27, 16, 2);
 
-SPIClass *hspi = NULL;
-Adafruit_BMP280 bmp(BMP_CS, HSPI_MOSI, HSPI_MISO, HSPI_SCK);
+OneWire onewire(ONE_WIRE);
+DallasTemperature ds18b20(&onewire);
 
 int lamp_state = HIGH;
 
-float end_time = 16.0;
 float timeline = 0;
+float time_step = 0.05;
 float temperature_func(float t) {
-    return 45 + 2 * pow(2.71828, 0.23 * t) - 0.3 * t * t;
+    if(t <= 0.5f) {
+        // rising phase: 30 -> 45
+        float start = 30.0f;
+        float end = 45.0f;
+        float k = log(end - start + 1.0f);
+        return start + (pow(EULER, k * (t / 0.5f)) - 1.0f) / (pow(EULER, k) - 1.0f) * (end - start);
+    } else {
+        // falling phase: 45 -> 25
+        float start = 45.0f;
+        float end = 25.0f;
+        float k = log(start - end + 1.0f);
+        return end + (pow(EULER, k * ((1.0f - t) / 0.5f)) - 1.0f) / (pow(EULER, k) - 1.0f) * (start - end);
+    }
 }
 
-void setup() {
-    pinMode(LAMP, OUTPUT);
+float dt = 0;
+float get_temperature() {
+    dt = millis();
+    ds18b20.requestTemperatures();
+    dt = millis() - dt;
+    return ds18b20.getTempCByIndex(0);
+}
 
-    Serial.begin(115200);
-    delay(100);
-    Serial.println("serial init");
-
-    if(!bmp.begin()) {
-        Serial.println("cannot find a bmp280, check wiring");
-        while(true);
-    }
-
-    float f0 = temperature_func(0);
-    Serial.println("bmp280 init");
-    Serial.print("trying to get to f(0) = ");
-    Serial.println(f0);
-
-    float temp = bmp.readTemperature();
-    while(temp < f0 - 0.05 || temp > f0 + 0.05) {
-        Serial.print("curr ");
-        Serial.println(temp);
-        if(temp > f0) {
+void temperature_control(float target, bool hold) {
+    float temp = -1;
+    // keep adjust until reaching acceptable range
+    while(temp < target - 0.05 || temp > target + 0.05 || hold) {
+        temp = get_temperature();
+        lcd.setCursor(0, 1);
+        lcd.print("curr ");
+        lcd.print(temp);
+        if(temp > target) {
             lamp_state = LOW;
         } else {
             lamp_state = HIGH;
         }
         digitalWrite(LAMP, lamp_state);
 
-        delay(1000);
-        temp = bmp.readTemperature();
+        delay(INTERVAL - dt);
     }
+}
 
-    Serial.println("f0 reached, start aligning to f(t) ...");
+void setup() {
+    lcd.init();
+    lcd.backlight();
+
+    ds18b20.begin();
+
+    pinMode(LAMP, OUTPUT);
 }
 
 void loop() {
-    float temp = bmp.readTemperature();
-    float target_temp = temperature_func(timeline);
-
-    Serial.print("curr ");
-    Serial.print(temp);
-    Serial.print(" targ ");
-    Serial.println(target_temp);
-
-    if(temp > target_temp) {
-        lamp_state = LOW;
-    } else {
-        lamp_state = HIGH;
+    float target = temperature_func(timeline);
+    // we have no cooling element
+    // TODO:
+    // change MIN_TEMP to ambient temp
+    if(target < MIN_TEMP) {
+        target = MIN_TEMP;
     }
 
-    digitalWrite(LAMP, lamp_state);
-    delay(1000);
-    if(timeline < end_time) {
-        timeline += 1.0 / 60.0;
-        if(timeline >= end_time) {
-            Serial.println("end time reached, holding temp");
-        }
+    lcd.setCursor(0, 0);
+    lcd.print("targ ");
+    lcd.print(target);
+
+    temperature_control(target, false);
+    timeline += time_step;
+
+    if(timeline >= 1.0) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("end point reached");
+        lcd.print(target);
+        temperature_control(target, true);
     }
 }
